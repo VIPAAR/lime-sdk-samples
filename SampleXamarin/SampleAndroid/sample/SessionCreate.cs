b8ef9c3cb37b4073443a8e418203d440f6cacd53
CalixHu <calix.hu@helplightning.com>
2019-12-09 16:20:27 +0800

    add fake server sample

---
+++ SampleXamarin/SampleAndroid/sample/SessionCreate.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace SampleAndroid
+{
+    [Obsolete]
+    public class SessionCreate : Fragment
+    {
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your fragment here
+        }
+
+        [Obsolete]
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            // Use this to return your custom view for this Fragment
+            return inflater.Inflate(Resource.Layout.fragment_session_created, container, false);
+        }
+    }
+}
