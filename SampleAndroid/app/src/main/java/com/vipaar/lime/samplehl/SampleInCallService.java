8b7babc82d5148878ff0309d6a9deeb5c5494a16
Scott Wehby <scott.wehby@helplightning.com>
2021-08-02 15:48:28 -0500

    update sdk and set SampleInCallService as HLClientDelegate so that it receives events from the call

--- SampleAndroid/app/src/main/java/com/vipaar/lime/samplehl/SampleInCallService.java
+++ SampleAndroid/app/src/main/java/com/vipaar/lime/samplehl/SampleInCallService.java
@@ -1,13 +1,21 @@
 package com.vipaar.lime.samplehl;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 
 import com.vipaar.lime.hlsdk.client.HLCall;
+import com.vipaar.lime.hlsdk.client.HLClient;
 import com.vipaar.lime.hlsdk.services.InCallService;
 
 import timber.log.Timber;
 
 public class SampleInCallService extends InCallService {
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        HLClient.getInstance().setHLClientDelegate(this);
+        return super.onStartCommand(intent, flags, startId);
+    }
 
     public void onCallEnded(HLCall call, String reason) {
         Timber.d("Sample onCallEnded");
